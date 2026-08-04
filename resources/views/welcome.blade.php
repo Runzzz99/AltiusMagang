379e938ec3324cadf768c16fd99b7ffea9b2bd0d
fahmisekali <daffaafrizalfahmi@gmail.com>
2026-08-04 09:40:04 +0700

    Menggganti navbar

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -54,7 +54,7 @@
 <body>
     <nav class="navbar navbar-expand-lg">
         <div class="container">
-            <a class="navbar-brand" href="{{ route('home') }}">Sistem HRD <small>Modul Rekrutmen &middot; Data Calon Karyawan</small></a>
+            <a class="navbar-brand" href="{{ route('home') }}">SISTEM PEREKRUTAN KARYAWAN <small> &middot;Informasi HRD</small></a>
             <a class="btn btn-accent btn-sm" href="{{ route('calon-karyawan.index') }}">Buka Aplikasi &raquo;</a>
         </div>
     </nav>
